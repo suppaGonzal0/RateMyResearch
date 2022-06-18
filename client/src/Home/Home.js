138f712d89bac2da7d598534164ceacaf81f0005
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-06-18 19:08:07 +0600

    Minor changes
    Removed fontawesome, imported react icons, some css changes

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
 import './Home.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faFilter } from '@fortawesome/free-solid-svg-icons'
+import { FaSearch, FaFilter } from 'react-icons/fa';
+import { FiBookmark } from 'react-icons/fi';
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
     //Fetch from database
 
     const [papers, setPapers] = useState([
-        {title: "Rumor Detection on Social Media: Datasets, Methods and Opportunities", category: "ML", rating: "4.5/5", date:"15th June, 2020"},
-        {title: "Remote sensing image denoising ", category: "IP", rating: "3/5", date:"10th August, 2021"},
-        {title: "Remote sensing image denoising ", category: "IP", rating: "3/5", date:"10th August, 2021"},
-        {title: "Remote sensing image denoising ", category: "IP", rating: "3/5", date:"10th August, 2021"}
+        {id:"1", title: "Rumor Detection on Social Media: Datasets, Methods and Opportunities", category: "ML", rating: "4.5/5", date:"15th June, 2020"},
+        {id:"2", title: "Remote sensing image denoising ", category: "IP", rating: "3/5", date:"10th August, 2021"},
+        {id:"3", title: "Remote sensing image denoising ", category: "IP", rating: "3/5", date:"10th August, 2021"},
+        {id:"4", title: "Remote sensing image denoising ", category: "IP", rating: "3/5", date:"10th August, 2021"}
     ])
 
   return (
@@ -21,13 +22,13 @@ const Home = () => {
 
         <div className='search'>
             <input type="text" placeholder='search a paper'/>
-            <button className='searchbtn'><FontAwesomeIcon icon={faSearch} /></button>
-            <button className='filterbtn'><FontAwesomeIcon icon={faFilter} /></button>
+            <button className='searchbtn'><FaSearch style={{ fill: '#edecff' }} fontSize="1em" /></button>
+            <button className='filterbtn'><FaFilter style={{ fill: '#edecff' }} fontSize="0.9em" /></button>
         </div>
 
         <div className='papers'>
             {papers.map((paper) => (
-                <div className='paper'>
+                <div className='paper' key={paper.id}>
                     <h3>{paper.title}</h3>
                     <div className='paperInfo'>
                         <div className='left'>
@@ -36,10 +37,12 @@ const Home = () => {
                         </div>
                         <div className='right'>
                             <p>{paper.rating}</p>
-                            <button className='save'>+</button>
+                            <button className='save'><FiBookmark style={{ fill: 'none', stroke: "#3f37c9", strokeWidth: "2.5"}} fontSize="2em" /></button>
                         </div>
                     </div>
-                    <button className='read'>Read</button>
+                        <Link to={`/paper/${paper.id}`}>
+                            <button className='button'>View</button>
+                         </Link>
                 </div>
             ))}
         </div>
