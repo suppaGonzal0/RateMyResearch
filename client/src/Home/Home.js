71bdd17f5da840bb433d40f885bb81d18dcaab44
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-06-28 17:01:40 +0600

    Implemented filter by category

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -9,14 +9,15 @@ const Home = () => {
     //Fetch from database
 
     const papers = [
-        {id:"1", title: "Rumor Detection on Social Media: Datasets, Methods and Opportunities", category: "ML", rating: "4.5/5", date:"15th June, 2020"},
-        {id:"2", title: "Remote sensing image denoising", category: "IP", rating: "3/5", date:"10th August, 2021"},
-        {id:"3", title: "Public Safety Networks: Enabling Mobility", category: "DL", rating: "3.5/5", date:"10th August, 2021"},
-        {id:"4", title: "Switch Fabric Technology", category: "SR", rating: "2/5", date:"10th August, 2021"}
+        {id:"1", title: "Rumor Detection on Social Media: Datasets, Methods and Opportunities", category: "Machine Learning", rating: "4.5/5", date:"15th June, 2020"},
+        {id:"2", title: "Remote sensing image denoising", category: "Image Processing", rating: "3/5", date:"10th August, 2021"},
+        {id:"3", title: "Public Safety Networks: Enabling Mobility", category: "Deep Learning", rating: "3.5/5", date:"10th August, 2021"},
+        {id:"4", title: "Switch Fabric Technology", category: "Speech Recognition", rating: "2/5", date:"10th August, 2021"}
     ]
 
     const [searchedPaper, setSearchedPaper] = useState('');
     const [action, setAction] = useState('title');
+    const [filter, setFilter] = useState();
 
     let data = papers.filter((paper) => {
         if(action === 'title'){
@@ -26,19 +27,24 @@ const Home = () => {
                 return paper
             }
         } else if (action === 'filter'){
-            return paper.category === "ML"
+            return paper.category === filter
         }
-        return papers
     })
 
-    if(action === 'sort'){
+    console.log(action)
+
+    const filterCategory = (value) => {
+        setFilter(value)
+        setAction("filter")
         data = papers.filter((paper) => {
-            return paper.category === "IP"
+            return paper.category === "Image Processing"
         })
     }
 
-    if (action === "all"){
-        data = papers
+    if(action === 'sort'){
+        data = papers.filter((paper) => {
+            return paper.category === "Image Processing"
+        })
     }
 
 
@@ -51,9 +57,17 @@ const Home = () => {
             <input type="text" placeholder='search a paper'
                 onChange={(event) => {
                     setSearchedPaper(event.target.value)
+                    setAction("title")
                 }}/>
-            <button className='searchbtn'><FaSearch style={{ fill: '#edecff' }} fontSize="1em" onClick={() => setAction('all')}/></button>
-            <button className='filterbtn'><FaFilter style={{ fill: '#edecff' }} fontSize="0.9em" onClick={() => setAction('filter')}/></button>
+            <button className='searchbtn'><FaSearch style={{ fill: '#edecff' }} fontSize="1em" onClick={() => window.location.reload()}/></button>
+            <button className='filterbtn'><FaFilter style={{ fill: '#edecff' }} fontSize="0.9em"/>
+            <div className="dropdown">
+                <p id="ml" onClick={() => filterCategory(document.getElementById("ml").innerHTML)}>Machine Learning</p>
+                <p id="dl" onClick={() => filterCategory(document.getElementById("dl").innerHTML)}>Deep Learning</p>
+                <p id="ip" onClick={() => filterCategory(document.getElementById("ip").innerHTML)}>Image Processing</p>
+                <p id="sr" onClick={() => filterCategory(document.getElementById("sr").innerHTML)}>Speech Recognition</p>
+                </div>
+            </button>
             <button className='sortbtn'><FaSortAmountDown style={{ fill: '#edecff' }} fontSize="0.9em" onClick={() => setAction('sort')}/></button>
         </div>
 
