df8478b9a3dda816a19e6f10b7ce148fdb3c9e56
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-08-03 21:31:14 +0600

    rating and re-rate button effects

--- client/src/PaperDetails/PaperDetails.js
+++ client/src/PaperDetails/PaperDetails.js
@@ -6,9 +6,20 @@ import { FiStar } from 'react-icons/fi';
 
 const PaperDetails = (paper) => {
 
-  const [rate, setRate] = useState()
+  const [rate, setRate] = useState(0)
+  const [hover, setHover] = useState()
+  const [star, setStar] = useState(true)
 
     const {id} = useParams();
+
+    const rateSubmit = () => {
+      setStar(false)
+    }
+
+    const reRate = () => {
+      setStar(true)
+      setRate(0)
+    }
 
     //dummy abstract:
     const abstract = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
@@ -34,35 +45,37 @@ const PaperDetails = (paper) => {
               <h4>Abstract</h4>
               <p>{filteredPaper.abstract}</p>
             </div>
-            <div className="besideAbstract">
-              <h3>Rating: 4/5</h3>
-              <button className='button'>Comment</button>
-              <button className='button'>Rate</button>
-              <button className='button'>Save</button>
-            </div>
           </div>
         </div>
       ))}
 
-      {/* <div className='rating'>
-        <FiStar className='star' onClick={() => setRate(1)}
-        style={{ stroke: '#3f37c9'}} fontSize="2.2em" 
-        onMouseOver={({target})=>target.style.fill="orange"}
-        onMouseOut={({target})=>target.style.fill="white"}/>
-        <FiStar className='star' style={{ stroke: '#3f37c9'}} fontSize="2.2em"
-        onMouseOver={({target})=>target.style.fill="orange"}
-        onMouseOut={({target})=>target.style.fill="white"} />
-        <FiStar className='star' style={{ stroke: '#3f37c9'}} fontSize="2.2em"
-        onMouseOver={({target})=>target.style.fill="orange"}
-        onMouseOut={({target})=>target.style.fill="white"} />
-        <FiStar className='star' style={{ stroke: '#3f37c9'}} fontSize="2.2em"
-        onMouseOver={({target})=>target.style.fill="orange"}
-        onMouseOut={({target})=>target.style.fill="white"} />
-        <FiStar className='star' style={{ stroke: '#3f37c9'}} fontSize="2.2em"
-        onMouseOver={({target})=>target.style.fill="orange"}
-        onMouseOut={({target})=>target.style.fill="white"} />
-      </div> */}
       
+        {star ? <>
+          <div className='rating'>
+        {[...Array(5)].map((star, i) => { 
+          const ratingValue = i + 1
+
+          return (
+            <label>
+              <input type="radio" name='rating' value={ratingValue} 
+              onClick={() => setRate(ratingValue)}/>
+              <FiStar className='star' size={40} stroke={"#3f37c9"}
+              fill = {ratingValue <= (hover || rate) ? "#ffc107" : "white"} 
+              onMouseEnter={()=> setHover(ratingValue)}
+              onMouseLeave={()=> setHover(null)}/>
+            </label>
+          )
+        })}
+      </div>
+      <button className='button' onClick={() => rateSubmit()}>Submit</button>
+      
+        </> : 
+        <>
+           <h3>You rated this paper {rate} out of 5!</h3>
+          <button className='button' onClick={reRate}>Re-rate</button>
+        </>}
+     
+
       <h2>Comments</h2>
 
     </div>
