429b84ecb63346728b1ff117e59ce70e73ca3cd8
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-08-10 23:19:13 +0600

    Image upload frontend

--- client/src/Profile/editSection/Edit.js
+++ client/src/Profile/editSection/Edit.js
@@ -1,10 +1,34 @@
 import React, { useState } from 'react'
 import "./Edit.css"
+import axios from 'axios'
 
 const Edit = () => {
 
   //set aboutme function
   const [aboutme, setAboutme] = useState("No information")
+  const [img, setImg] = useState({
+    file: [],
+  })
+  
+  const handleImg = (e) => {
+    setImg({
+      ...img,
+      file:e.target.files[0]
+    })
+  }
+
+  const submit = async () => {
+    const imgData = new FormData();
+    imgData.append("picture", img.file)
+
+    axios.post("http://localhost:3001/img", imgData, {
+      headers: {"Content-Type": "multipart/img-data"}
+    })
+    .then(res => {
+      console.warn(res)
+    })
+
+  }
 
   return (
     <div className='editSection'>
@@ -16,7 +40,8 @@ const Edit = () => {
         </div>
         <div className="dpChange">
           <h3>Change profile picture</h3>
-          <button>Change</button>
+          <input type="file" className='imgInput' onChange={handleImg}/>
+          <button type='submit' onClick={() => submit()}>Change</button>
         </div>
     </div>
   )
