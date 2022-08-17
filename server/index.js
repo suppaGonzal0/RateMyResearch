f015052386f236d195ba7b8380fc4b3262c8470d
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-08-17 15:27:36 +0600

    connected mongodb, set up middlewares and error handling

---
+++ server/index.js
@@ -0,0 +1,43 @@
+import express from "express"
+import mongoose from "mongoose"
+import authRoute from "./routes/AuthRoute.js"
+import usersRoute from "./routes/UsersRoute.js"
+import papersRoute from "./routes/PapersRoute.js"
+import requestsRoute from "./routes/RequestsRoute.js"
+import cookieParser from "cookie-parser"
+
+
+const app = express()
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/ratemyresearch");
+        console.log("connected to db")
+    } catch (error) {
+        throw error
+    }
+}
+
+//middlewares
+app.use(cookieParser())
+app.use(express.json());
+
+app.use("/auth", authRoute)
+app.use("/users", usersRoute)
+app.use("/papers", papersRoute)
+app.use("/requests", requestsRoute)
+
+app.use((err, req, res, next) => {
+    const errStatus = err.status || 500
+    const errMsg = err.message || "Something went wrong."
+    return res.status(errStatus).json({
+        success: false,
+        status: errStatus,
+        message: errMsg
+      });
+})
+
+app.listen(3001, () => {
+    connectDB()
+    console.log(("connected to server"));
+})
