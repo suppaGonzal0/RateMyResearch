90584ece3325216e84dce96f1727614ae30d6279
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-08-18 15:07:41 +0600

    Renamed AddPapers to PaperRequests

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from './Home/Home';
 import PaperDetails from './PaperDetails/PaperDetails';
 import Request from './Request/Request';
 import Profile from './Profile/Profile';
-import AddPapers from './AddPapers/AddPapers';
+import PaperRequests from './PaperRequests/PaperRequests';
 import Login from './Login/Login';
 import UserList from './UserList/UserList';
 import Unauthorized from './Unauthorized/Unauthorized';
@@ -19,20 +19,19 @@ function App() {
   return (
     <div className="App">
         <BrowserRouter>
-          <Navbar admin={admin}/>
+          {userLoggedIn && <Navbar admin={admin}/>}
             <Routes>
               {userLoggedIn ? 
                 <>
                   <Route path="/" element={<Home admin={admin}/>}/>
                   <Route path="/paper/:id" element={<PaperDetails/>}/>
-                  <Route path="/request" element={<Request/>}/>
+                  <Route path="/request" element={<Request admin={admin}/>}/>
                   <Route path="/profile" element={<Profile/>}/>
-                  <Route path="/add" element={<AddPapers/>}/>
+                  <Route path="/paperReq" element={<PaperRequests/>}/>
                   <Route path="/userlist" element={<UserList/>}/>
                   <Route path="/unauthorized" element={<Unauthorized />}/>
-                  <Route path="/" element={<Home />}/>
                 </> : 
-                  <Login/>}
+                  <Route path= "*" element={<Login />}/>}
               </Routes>
             </BrowserRouter>  
      </div>
